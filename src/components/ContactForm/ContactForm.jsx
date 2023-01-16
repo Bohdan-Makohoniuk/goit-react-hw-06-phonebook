5315c736556f5fa884084f7580c893a78712dccf
Bohdan <makohoniuk.bo@gmail.com>
2023-01-16 21:48:10 +0200

    add_newContactForm

---
+++ src/components/ContactForm/ContactForm.jsx
@@ -0,0 +1,73 @@
+import React, { useState } from 'react';
+import { toast } from 'react-hot-toast';
+import { useDispatch, useSelector } from 'react-redux';
+
+import toast, { Toaster } from 'react-hot-toast';
+import { Button, TextField } from '@mui/material';
+import { IoIosAddCircle } from 'react-icons/io';
+
+import { addContact } from 'redux/contactsSlice';
+import { getContacts } from 'redux/selectors';
+
+export const ContactForm = () => {
+  const [name, setName] = useState('');
+  const [number, setNumber] = useState('');
+
+  const contacts = useSelector(getContacts);
+  const dispatch = useDispatch();
+
+  const onFormChange = e => {
+    const { name, value } = e.target;
+    switch (name) {
+      case 'name':
+        setName(value);
+        break;
+      case 'number':
+        setNumber(value);
+        break;
+      default:
+        throw new Error('There has been a mistake. Try again, please');
+    }
+  };
+
+  const onFormSubmit = e => {
+    e.preventDefault();
+    const checkName = contacts.find(
+      contacts => contacts.name.toLowerCase() === name.toLowerCase()
+    );
+      if (checkName) {
+          toast.error(`${name} is already in your contacts`);
+          return;
+      } else {
+          dispatch(addContact(name, number));
+      }
+      setName('');
+      setNumber('');
+  };
+    return (
+        <Form onSubmit={onFormSubmit}>
+            <TextField
+                id="outlined-basic"
+                label="Name"
+                variant="outlined"
+                size="small"
+                type="text"
+                name="name"
+                value={name}
+                pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+                title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+                required
+                onChange={onFormChange}
+            />
+            <Button
+                type="submit"
+                variant="outlined"
+                size="small"
+                startIcon={<IoIosAddCircle />}
+            >
+                Add contact
+            </Button>
+            <Toaster />
+        </Form>
+    );
+};
--- src/index.js
+++ src/index.js
@@ -3,8 +3,20 @@ import ReactDOM from 'react-dom/client';
 import { App } from 'components/App';
 import './index.css';
 
+import { PersistGate } from 'redux-persist/integration/react';
+import { Provider } from './redux/store';
+import { ThemeProvider } from 'styled-components';
+import { store, persistor } from './redux/store';
+import { theme } from './theme';
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <App />
+        </PersistGate>
+        </Provider>
+      </ThemeProvider>
   </React.StrictMode>
 );
